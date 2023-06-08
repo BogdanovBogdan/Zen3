c1010ab302914dd668d1d546487feea5cf2363ac
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2023-06-08 12:43:34 +0700

    Change default styles of root & body

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,6 +1,9 @@
 import {
+  alignItems,
   classnames,
   container,
+  display,
+  flexDirection,
   margin,
   maxWidth,
   padding,
@@ -8,6 +11,9 @@ import {
 import ChildrenProp from 'models/ChildrenProp'
 
 const root = classnames(
+  display('flex'),
+  alignItems('items-center'),
+  flexDirection('flex-col'),
   container('container'),
   margin('mx-auto'),
   padding('pb-10', 'py-4'),
