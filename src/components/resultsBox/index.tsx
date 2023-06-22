91eb5704658244be1786698162fbcd4a5359ec52
Bogdan Bogdanov <bogdanovbogdanv@gmail.com>
2023-06-22 08:34:30 +0700

    Limit the height of the results box

--- src/components/resultsBox/index.tsx
+++ src/components/resultsBox/index.tsx
@@ -2,12 +2,41 @@ import { useSnapshot } from 'valtio'
 import DBStore from 'stores/DBStore'
 import List from 'components/resultsBox/List'
 import NotFound from 'components/resultsBox/NotFound'
+import classnames, {
+  alignItems,
+  backgroundColor,
+  borderRadius,
+  display,
+  flexDirection,
+  gap,
+  margin,
+  maxHeight,
+  overflow,
+  padding,
+  position,
+  width,
+} from 'classnames/tailwind'
+
+const container = classnames(
+  position('relative'),
+  display('flex'),
+  flexDirection('flex-col'),
+  width('w-176'),
+  maxHeight('max-h-98'),
+  backgroundColor('bg-white'),
+  borderRadius('rounded-2xl'),
+  margin('m-2'),
+  gap('gap-4'),
+  alignItems('items-center'),
+  overflow('overflow-hidden'),
+  padding('p-6')
+)
 
 export default function ResultsBox() {
   const { filtered } = useSnapshot(DBStore)
 
   return (
-    <div className="w-176 bg-white m-2 rounded-2xl p-6 relative flex flex-col gap-4 items-center">
+    <div className={container}>
       {filtered.length ? <List list={filtered} /> : <NotFound />}
     </div>
   )
